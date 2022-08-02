972b65d6cd0fb4467efa184a68a1015156ef8320
omeryildizce <omeryildizce@gmail.com>
2022-08-02 15:22:04 +0300

    Bootstrap

--- examples/myapp/views.py
+++ examples/myapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 import datetime
 data = {
-    "telefon":["samsung s20", "samsung s21"],
+    "telefon":["samsung s20", "samsung s21","Iphone 12"],
     "bilgisayar":["laptop 1", "laptop 2"],
     "elektronik":[ ], 
 }
