96d59b9e59fec1bdd4b0794d57c9698740657399
omeryildizce <omeryildizce@gmail.com>
2022-08-02 16:04:03 +0300

    Urls and Views

--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Anasayfa")
+
+def movies(request):
+    return HttpResponse("Filmler")
+    
+
+def movie_details(request, slug):
+    return HttpResponse("Film detayı:"+ slug)
+    
