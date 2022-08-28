bad06dd77ae64dca70cff23d20f788a1d214dfb5
omeryildizce <omeryildizce@gmail.com>
2022-08-28 17:14:07 +0300

    Model relations

--- movieapp2/movies/models.py
+++ movieapp2/movies/models.py
@@ -1,23 +1,12 @@
-from pyexpat import model
 from django.db import models
 from django.core.validators import MinLengthValidator
 
 class Genre(models.Model):
     name = models.CharField(max_length=100)
 
-class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(validators=[MinLengthValidator(20)])
-    image_name = models.CharField(max_length=100)
-    image_cover = models.CharField(max_length=100)
-    date = models.DateField()
-    slug = models.SlugField(unique=True, db_index=True)
-    budget = models.DecimalField(max_digits=19, decimal_places=2)
-    language = models.CharField(max_length=100)
 class Contact(models.Model):
     address = models.CharField(max_length=200)
     email = models.EmailField()
-
 
 class Person(models.Model):
     genders = (
@@ -38,8 +27,22 @@ class Person(models.Model):
     date_of_birth = models.FloatField()
     gender = models.CharField(max_length=1, choices=genders)
     duty_type = models.CharField(max_length=1, choices=duty_types)
+    contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null = True, blank=True)
+    
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(validators=[MinLengthValidator(20)])
+    image_name = models.CharField(max_length=100)
+    image_cover = models.CharField(max_length=100)
+    date = models.DateField()
+    slug = models.SlugField(unique=True, db_index=True)
+    budget = models.DecimalField(max_digits=19, decimal_places=2)
+    language = models.CharField(max_length=100)
+    people = models.ManyToManyField(Person)
+    genres = models.ManyToManyField(Genre)
 
 class Video(models.Model):
     title = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
-    
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
