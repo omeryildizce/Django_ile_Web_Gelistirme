0885200de11b36fc91988ddf3d4c9ab48aa14f5b
omeryildizce <omeryildizce@gmail.com>
2022-11-13 14:20:59 +0300

    Form Templates

--- bolum_02_Forms/examples/myapp/forms.py
+++ bolum_02_Forms/examples/myapp/forms.py
@@ -3,9 +3,12 @@ from django import forms
 
 class ProductCrteateForm(forms.Form):
     product_name = forms.CharField(
-        label="Ürün Adı", required=False, min_length=3, max_length=20, error_messages={"min_length": "min 3 karakter giriniz.", "max_length": "mak 20 karakter giriniz."})
-    price = forms.DecimalField(label="Fiyat", min_value=10, max_value=10000)
+        label="Ürün Adı" , min_length=3, max_length=20, error_messages={"min_length": "min 3 karakter giriniz.", "max_length": "mak 20 karakter giriniz."}, widget=forms.TextInput(attrs={"class": "form-control"}))
+    price = forms.DecimalField(label="Fiyat", min_value=10, max_value=10000,
+                               widget=forms.TextInput(attrs={"class": "form-control"}))
     description = forms.CharField(
-        label="Ürün Açıklaması", widget=forms.Textarea())
-    imageUrl = forms.CharField(label="Fotağraf")
-    slug = forms.SlugField(label="Url")
+        label="Ürün Açıklaması",  widget=forms.Textarea(attrs={"class": "form-control"}))
+    imageUrl = forms.CharField(
+        label="Fotağraf", widget=forms.TextInput(attrs={"class": "form-control"}))
+    slug = forms.SlugField(label="Url", widget=forms.TextInput(
+        attrs={"class": "form-control"}))
