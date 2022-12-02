622df13124f2a124c909205bc0cfaf95e10f0056
omeryildizce <omeryildizce@gmail.com>
2022-12-02 11:40:24 +0300

    User Login

--- bolum_03_Membership/account/views.py
+++ bolum_03_Membership/account/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import redirect, render
-
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
-def login(request):
-    return render(request, "account/login.html")
+def login_request(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password= password)
+        if user is not None:
+            login(request, user)
+            return redirect("products")
+        else:
+            return render(request, "account/login.html", {
+                "error":"kullanıcı adı veya parola yanlış."
+            })
+    else:
+        return render(request, "account/login.html")
 
-def register(request):
-    return render(request, "account/register.html")
+def register_request(request):
+    if request.method == "POST":
+        #login
+        pass
+    else:
+        return render(request, "account/register.html")
 
-def logout(request):
+def logout_request(request):
     return redirect("products")
