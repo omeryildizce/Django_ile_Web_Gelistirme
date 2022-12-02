c0dea06068189e933e757a78f853c5b97787c776
omeryildizce <omeryildizce@gmail.com>
2022-12-02 12:25:47 +0300

    User Register

--- bolum_03_Membership/account/views.py
+++ bolum_03_Membership/account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
-# Create your views here.
+from django.contrib.auth.models import User
+
 
 def login_request(request):
     if request.user.is_authenticated:
@@ -8,23 +9,40 @@ def login_request(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(request, username=username, password= password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect("products")
         else:
             return render(request, "account/login.html", {
-                "error":"kullanıcı adı veya parola yanlış."
+                "error": "Kullanıcı adı veya parola yanlış."
             })
     else:
         return render(request, "account/login.html")
 
+
 def register_request(request):
     if request.method == "POST":
-        #login
-        pass
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password == repassword:
+            if User.objects.filter(username=username).exists():
+                return render(request, "account/register.html", {"error": "Username kullanılıyor."})
+            else:
+                if User.objects.filter(email = email).exists():
+                    return render(request, "account/register.html", {"error": "Email kullanılıyor."})
+                else:
+                    user = User.objects.create_user(username=username, email=email, password=password)
+                    user.save()
+                    return redirect("login")
+        else:
+            return render(request, "account/register.html", {"error": "Parola eşleşmiyor."})
     else:
         return render(request, "account/register.html")
+
 
 def logout_request(request):
     logout(request)
